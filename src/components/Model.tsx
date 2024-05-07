9215359d745af8b303983a7a90b84138641c9434
Max Rogers <maxrogerssepulveda@gmail.com>
2024-05-06 21:10:46 -0400

    fix: build console.log

--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -30,6 +30,8 @@ const Model = () => {
   const [smallRotation, setSmallRotation] = useState(0);
   const [largeRotation, setLargeRotation] = useState(0);
 
+  console.log(smallRotation, largeRotation);
+
   useGSAP(() => {
     gsap.to('#heading', { opacity: 1, y: 0 });
   }, []);
